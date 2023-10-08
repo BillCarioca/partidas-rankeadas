15c636cea73b9df90c054b10b27c9026024686f3
Bill Carioca <billcarioca@hotmail.com>
2023-10-08 09:37:28 -0300

    first commit

---
+++ rankeadas.js
@@ -0,0 +1,24 @@
+const heroi ={
+    nome:"Arthur",
+    vitorias: 50,
+    derotas:4
+}
+
+
+function calcularSaldo(vitorias,derotas){
+    return vitorias-derotas
+}
+function calcularNivel(saldo){
+    switch(true){
+        case saldo<=10: return "Ferro"
+        case saldo<=20: return "Bronze"
+        case saldo<=50: return "Prata"
+        case saldo<=80: return "Ouro"
+        case saldo<=90: return "Diamante"
+        case saldo<=100: return "Lendário"
+        case saldo>100: return "Imortal"
+    }   
+}
+const saldo = calcularSaldo(heroi.vitorias,heroi.derotas)
+const nivel = calcularNivel(saldo)
+console.log(`O Herói de nome ${heroi.nome} tem de saldo de ${saldo} está no nível de ${nivel}`)
